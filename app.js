efdd16e8cb2fe5beed6c5fbc0e69dafe22a426a7
Sharjeel Sultan <sharjeelsultan27@gmail.com>
2022-09-09 10:21:56 +0500

    integerated backend

--- app.js
+++ app.js
@@ -1,1 +1,24 @@
 document.getElementById("regTab").style = "background-color: rgba(27, 27, 50, 0.8) !important;";
+
+
+
+document.getElementById("submit").addEventListener("click", () => {   
+    //function click(){
+    var city = document.getElementById("city").value;
+    var course = document.getElementById("course").value;
+    var name = document.getElementById("name").value;
+    var fatherName = document.getElementById("fname").value;
+    var CNIC = document.getElementById("cnic").value;
+    var FatherCNIC = document.getElementById("fcnic").value;
+    var contact = document.getElementById("contact").value;
+    var mail = document.getElementById("email").value;
+    console.log(name+" "+course+" "+CNIC+" "+"/n"+fatherName+" "+FatherCNIC+" "+"/n"+" "+contact+" "+mail);
+    localStorage.setItem("StudentName",name);
+    localStorage.setItem("fatherName",fatherName);
+    localStorage.setItem("CNIC", CNIC);
+    localStorage.setItem("fathersCNIC",FatherCNIC);
+    localStorage.setItem("course", course);
+    localStorage.setItem("city",city);
+    localStorage.setItem("contact", contact);
+    localStorage.setItem("mail", mail);
+})
--- card.js
+++ card.js
@@ -1,1 +1,26 @@
 document.getElementById("idTab").style = "background-color: rgba(27, 27, 50, 0.8) !important;";
+
+var getName, getFname, getCNIC, getFCNIC, getCity, getCourse, getContact, getMail;
+
+getName = localStorage.getItem("StudentName");
+getCNIC = localStorage.getItem("CNIC");
+getFname = localStorage.getItem("fatherName");
+getFCNIC = localStorage.getItem("fathersCNIC");
+getCity = localStorage.getItem("city");
+getCourse = localStorage.getItem("course");
+getContact = localStorage.getItem("contact");
+getMail = localStorage.getItem("mail");
+
+document.getElementById("name").innerHTML = getName;
+document.getElementById("idnumber").innerHTML = getCNIC;
+document.getElementById("course").innerHTML = getCourse;
+var theDate = new Date();
+const day = theDate.getDate();
+const month = theDate.getMonth() + 1; // getMonth() returns month from 0 to 11
+const year = theDate.getFullYear();
+
+const curDate = `${day}/${month}/${year}`;
+const expDate = `${day}/${month}/${year+1}`
+
+document.getElementById("issuedate").innerHTML = "ISSUED AT:  "+ curDate;
+document.getElementById("expdate").innerHTML = "VALID TILL:  "+ expDate;
